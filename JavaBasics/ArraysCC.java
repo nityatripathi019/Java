60fd8d408176d7d235210a443a6d0c2aa158ae88
Nitya Tripathi <diyatripathi70@gmail.com>
2025-06-03 18:28:56 +0530

    kadans algorithm & trappedwater

--- JavaBasics/ArraysCC.java
+++ JavaBasics/ArraysCC.java
@@ -73,16 +73,73 @@ public class ArraysCC {
     System.out.println("total subarrays: " + ts);
   }
 
-  public static void main(String args[]) {
-    int numbers[] = { 1, 3, 5, 7, 8, 19, 10 };
-    // int result = BinarySearch(numbers, 3);
-    // System.out.println("element found at index : " + (result + 1));
-    // System.out.println("array after revrse");
-    // reverseArray(numbers);
-    // System.out.println("array pairing");
-    // arrayPairs(numbers);
+  public static void maxsubarraysum(int numbers[]) {
+    int max = Integer.MIN_VALUE; // -infinity
+    for (int i = 0; i < numbers.length; i++) {
+      int start = i;
+      for (int j = i; j < numbers.length; j++) {
+        int end = j;
+        int currSum = 0;
+        for (int k = start; k <= end; k++) {
+          currSum += numbers[k];
+          System.out.println(currSum);
+          if (max < currSum) {
+            max = currSum;
 
-    System.out.println("print subarrays");
-    printSubArrays(numbers);
+          }
+        }
+      }
+    }
+    System.out.println("Max subarraysum : " + max);
+  }
+
+  public static void kadans(int numbers[]) {
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    for (int i = 0; i < numbers.length; i++) {
+      currSum += numbers[i];
+      if (currSum < 0) {
+        currSum = 0;
+      }
+      if (maxSum < currSum) {
+        maxSum = currSum;
+      }
+    }
+    System.out.println("maxsum : " + maxSum);
+  }
+
+  // TrapRainwater
+  public static void trapRainwater(int height[]) {
+    int n = height.length;
+
+    // calculate left max boundary
+    int leftMax[] = new int[n];
+    leftMax[0] = height[0];
+    for (int i = 1; i < n; i++) {
+      leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    }
+    // calculate right max boundary
+    int rightMax[] = new int[n];
+    rightMax[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+      rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+    }
+
+    // calculate waterlevel = min(leftmax.rightmax)
+    int trappedwater = 0;
+    for (int i = 0; i < n; i++) {
+      int waterlevel = Math.min(leftMax[i], rightMax[i]);
+      trappedwater += waterlevel - height[i];
+
+    }
+    System.out.println(trappedwater);
+  }
+
+  public static void main(String args[]) {
+    // int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+    // kadans(numbers);
+    int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+    trapRainwater(height);
+
   }
 }
