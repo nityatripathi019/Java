9e2485185c03cb9ac9efeea569a749ce1bfbcb23
Nitya Tripathi <diyatripathi70@gmail.com>
2025-08-05 15:41:33 +0530

    add chnages

--- JavaBasics/Classroom.java
+++ JavaBasics/Classroom.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
 
+import java.util.Set;
+
 public class Classroom {
 
   public static void main(String args[]) {
     // create
-    HashMap<String, Integer> hm = new HashMap<>();
-    // insertng data timecomplexity O(1)
-    hm.put("India", 150);
-    hm.put("China", 100);
-    hm.put("Australia", 50);
+    // HashMap<String, Integer> hm = new HashMap<>();
+    // // insertng data timecomplexity O(1)
+    // hm.put("India", 150);
+    // hm.put("China", 100);
+    // hm.put("Australia", 50);
 
-    System.out.println(hm);
+    // System.out.println(hm);
 
     // get O(1)
     // int population = hm.get("India");
@@ -30,10 +32,36 @@ public class Classroom {
     // System.out.println(hm);
 
     // Size
-    System.out.println(hm.size());
+    // System.out.println(hm.size());
     // Is Empty
-    System.out.println(hm.isEmpty());
+    // System.out.println(hm.isEmpty());
     // clear it will clear whole hashmap
+    // hm.clear();
+    // System.out.println(hm.isEmpty());
 
+    // Iteration on Hashmap O(1)
+    HashMap<String, Integer> hm = new HashMap<>();
+    hm.put("India", 100);
+    hm.put("China", 150);
+    hm.put("US", 50);
+    hm.put("Indonesia", 10);
+    hm.put("Nepal", 5);
+    System.out.println(hm);
+    // keySet()
+    Set<String> keys = hm.keySet();
+    System.out.println(keys);// [india,china,us..]
+
+    for (String k : keys) {
+      System.out.println(k + " " + hm.get(k));
+    }
+    // we can also find max and min
+    int max = Integer.MIN_VALUE;
+    for (String k : keys) {
+      if (max < hm.get(k)) {
+        max = hm.get(k);
+      }
+
+    }
+    System.out.println("maximum value:" + max);
   }
 }
