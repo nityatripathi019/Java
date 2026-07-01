50efe617c8e6ae829fc7daf865d3e9ca2a1fd8e4
Nitya Tripathi <diyatripathi70@gmail.com>
2026-07-01 14:59:36 +0530

    add new files

---
+++ LinkedList.java
@@ -0,0 +1,69 @@
+
+public class LinkedList {
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  public void addFirst(int data) {
+    // create new node
+    Node newNode = new Node(data);
+
+    if (head == null) {
+      head = tail = newNode;
+      return;
+    }
+
+    // step2- newNode next=head
+    newNode.next = head;// linking
+    // step3 -head = newnode
+    head = newNode;
+  }
+
+  public void addLast(int data) {// O(1)
+    // create a node
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = tail = newNode;
+      return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
+
+  public void print() {
+    if (head == null) {
+      System.out.println("ll is empty");
+      return;
+    }
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.data + "->");
+      temp = temp.next;
+    }
+    System.out.println("null");
+  }
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList();
+    ll.print();
+    ll.addFirst(1);
+    ll.print();
+    ll.addFirst(2);
+    ll.print();
+    ll.addFirst(3);
+    ll.print();
+    ll.addLast(4);
+    ll.print();
+    ll.addLast(5);
+    ll.print();
+  }
+}
